82b6b687d4d3a310df2358fa243206515d5135ef
temp4git <temp4git@example.com>
2020-03-01 19:14:24 +0800

    [WIP] add ner & tfidf filter

--- main.py
+++ main.py
@@ -1,75 +1,24 @@
-import re
-import os
-import json
-import docx
-import glob2
-from pathlib import Path
+# [WIP]
+from utils import *
+from sklearn.feature_extraction.text import TfidfVectorizer
 
+docs = []
+file_path_list = glob2.glob("./data/*")
 
-output = dict()
-
-
-def get_stop_words():
-    """
-    載入停用詞
-    """
-    with open("stopwords-en.txt", "r", encoding="utf8") as f:
-        stop_words = f.readlines()
-        stop_words = [sw.replace("\n", "") for sw in stop_words]
-    return stop_words
-
-
-def get_content(file_path):
-    """
-    獲取檔案內文
-    input: 檔案路徑
-    output: 檔案內文
-    """
-    if Path(file_path).suffix == ".docx":
-        paragraphs = docx.Document(file_path).paragraphs
-        return " ".join([paragraph.text for paragraph in paragraphs])
-
-    elif Path(file_path).suffix in [".csv", ".txt"]:
-        for encoding in ["utf8", "big5", "latin-1"]:
-            try:
-                with open(file_path, encoding=encoding) as f:
-                    content = f.read()
-                return content
-            except:
-                pass
-
-    else:
-        raise NotImplementedError
-
-
-def get_result(content):
-    """
-    依照需求過濾檔案內文
-    input: 原始的檔案內文
-    output: 過濾後的檔案內文
-    """
-    # 去除停用詞
-    stop_words = get_stop_words()
-    _content = " ".join(
-        list(filter(lambda word: word not in stop_words, content.split(" ")))
-    )
-    # 其他過濾規則
-    _content = re.sub(
-        "\s", " ", _content
-    )  # 避免換行符去除後 造成兩詞相連 (不保留換行等符號 但需保留詞間空格)
-    _content = re.sub("[^\w| ]", "", _content)  # 只保留空格、英數字
-    while re.search(" " * 2, _content):  # 詞間最多只保留一個空格
-        _content = re.sub("  ", "", _content)
-    return _content
-
-
-for file_path in glob2.glob("./data/*"):
+for file_path in file_path_list:
     print(f"正在處理 {file_path}...")
-    # name = Path(file_path).stem
     name = os.path.basename(file_path)
-    content = get_content(file_path)
-    result = get_result(content)
-    if not os.path.exists("./output"):
-        os.mkdir("./output")
-    with open(f"./output/{name}.txt", "w", encoding="utf8") as f:
-        f.write(result)
+    content = get_raw_content(file_path)
+    content = get_regex_content(content)
+    content = get_ner_content(content)
+    docs.append(content)
+
+# TF-IDF
+vectorizer = TfidfVectorizer(token_pattern=r"(?u)\b\w+\b", norm=None)
+X = vectorizer.fit_transform(docs)
+feature_names = np.array(vectorizer.get_feature_names())
+sorted_indexes = X.toarray().argsort()
+for idx, indexes in enumerate(sorted_indexes):
+    print(f"==={file_path_list[idx]}===")
+    print(feature_names[indexes][:50])  # tfidf前50小 (過濾掉)
+    print(feature_names[indexes][::-1][:50])  # tfidf前50大 (關鍵字)
